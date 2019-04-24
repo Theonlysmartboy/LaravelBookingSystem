b60d5912d63959610a94c3f4c848115102f7f686
TheOnlySmartBoy <o2jose43@gmail.com>
2019-04-24 16:27:04 +0300

    Edited the admin_design blade

--- resources/views/layouts/adminlayout/admin_design.blade.php
+++ resources/views/layouts/adminlayout/admin_design.blade.php
@@ -3,8 +3,25 @@
     <head>
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/backend_images/favicon.png')}}">
-        <title>MFarmer Dashboard</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{asset('images/backend_images/icons/apple-icon-57x57.png')}}"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="{{asset('images/backend_images/icons/apple-icon-60x60.png')}}"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/backend_images/icons/apple-icon-72x72.png')}}"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="{{asset('images/backend_images/icons/apple-icon-76x76.png')}}"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="{{asset('images/backend_images/icons/apple-icon-114x114.png')}}"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="{{asset('images/backend_images/icons/apple-icon-120x120.png')}}"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="{{asset('images/backend_images/icons/apple-icon-144x144.png')}}"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="{{asset('images/backend_images/icons/apple-icon-152x152.png')}}"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('images/backend_images/icons/apple-icon-180x180.png')}}"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('images/backend_images/icons/android-icon-192x192.png')}}"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/backend_images/icons/favicon-32x32.png')}}"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="{{asset('images/backend_images/icons/favicon-96x96.png')}}"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/backend_images/icons/favicon-16x16.png')}}"/>
+        <link rel="manifest" href="{{asset('images/backend_images/icons/manifest.json')}}"/>
+        <meta name="msapplication-TileColor" content="#2E363F"/>
+        <meta name="msapplication-TileImage" content="{{asset('images/backend_images/icons/ms-icon-144x144.png')}}"/>
+        <meta name="theme-color" content="#2E363F"/>
+        <title>Admin Dashboard</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="{{asset('css/backend_css/bootstrap.min.css')}}" />
