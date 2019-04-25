b7162259a42e660eb9c3c4bde215bdd8242d994c
TheOnlySmartBoy <o2jose43@gmail.com>
2019-04-25 13:40:19 +0300

    Completed Admin Booking activities

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
  */
 
 Route::get('/', function () {
-   return redirect('/login');
+    return redirect('/login');
 });
 //Admin Routes
 Route::match(['get', 'post'], '/admin', 'AdminController@login');
@@ -21,22 +21,26 @@ Route::match(['get', 'post'], '/admin/update-pwd', 'AdminController@updatePasswo
 Route::get('/logout', 'AdminController@logout');
 Route::get('/admin/dashboard', 'AdminController@dashboard');
 Route::get('/admin/check-pwd', 'AdminController@chkPassword');
+//Admin booking routes
+Route::get('/admin/bookings', 'BookingController@viewAllBookings');
+Route::match(['get', 'post'], '/admin/accept_booking/{id}', ['middleware' => 'auth', 'uses' => 'BookingController@acceptBooking']);
+Route::match(['get', 'post'], '/admin/reject_booking/{id}', ['middleware' => 'auth', 'uses' => 'BookingController@rejectBooking']);
+Route::match(['get', 'post'], '/admin/delete_booking/{id}', 'BookingController@deleteBooking');
+
 //Client's Logout route
 Route::get('client/logout', 'BookingController@logout');
+
 //Client's Booking routes
-Route::match(['get', 'post'], '/client/add_booking',['middleware' => 'auth', 'uses' => 'BookingController@book']);
-Route::get('/client/view_bookings',['middleware' => 'auth', 'uses' => 'BookingController@viewBookings']);
-Route::match(['get', 'post'], '/client/edit_booking/{id}',['middleware' => 'auth', 'uses' => 'BookingController@editCategory']);
-Route::match(['get', 'post'], '/client/delete_booking/{id}',['middleware' => 'auth', 'uses' => 'BookingController@deleteCategory']);
+Route::match(['get', 'post'], '/client/add_booking', ['middleware' => 'auth', 'uses' => 'BookingController@book']);
+Route::get('/client/view_bookings', ['middleware' => 'auth', 'uses' => 'BookingController@viewBookings']);
+Route::match(['get', 'post'], '/client/edit_booking/{id}', ['middleware' => 'auth', 'uses' => 'BookingController@editCategory']);
+Route::match(['get', 'post'], '/client/delete_booking/{id}', ['middleware' => 'auth', 'uses' => 'BookingController@deleteCategory']);
+
 //Client's payment routes
-Route::match(['get', 'post'], '/client/make_payment/{id}',['middleware' => 'auth', 'uses' => 'BookingController@pay']);
-Route::get('/client/view_payments',['middleware' => 'auth', 'uses' => 'BookingController@viewPayments']);
-Route::get('/client/invoices',['middleware' => 'auth', 'uses' => 'BookingController@viewInvoices']);
-//Admin's Booking routes
-Route::match(['get', 'post'], '/admin/booking', 'BookingController@book');
-Route::get('/admin/view_bookings', 'BookingController@viewBookings');
-Route::match(['get', 'post'], '/admin/edit_booking/{id}', 'BookingController@editCategory');
-Route::match(['get', 'post'], '/admin/delete_booking/{id}', 'BookingController@deleteCategory');
+Route::match(['get', 'post'], '/client/make_payment/{id}', ['middleware' => 'auth', 'uses' => 'BookingController@pay']);
+Route::get('/client/view_payments', ['middleware' => 'auth', 'uses' => 'BookingController@viewPayments']);
+Route::get('/client/invoices', ['middleware' => 'auth', 'uses' => 'BookingController@viewInvoices']);
+
 Auth::routes();
-Route::get('pdfview',array('as'=>'pdfview','uses'=>'BookingController@pdfview'));
+Route::get('pdfview', array('as' => 'pdfview', 'uses' => 'BookingController@pdfview'));
 Route::get('/home', 'HomeController@index')->name('home');
