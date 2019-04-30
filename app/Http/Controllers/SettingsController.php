edcc57bdc801e547b842e456d91b990110c13669
TheOnlySmartBoy <o2jose43@gmail.com>
2019-04-30 12:54:32 +0300

    Completed the add account logic

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -30,6 +30,26 @@ class SettingsController extends Controller {
         }
     }
 
+    public function addAccount(Request $request) {
+        if (Session::has('adminSession')) {
+            if ($request->isMethod('post')) {
+                $data = $request->all();
+                $bank = new Setting;
+                $bank->bank = $data['product_name'];
+                $bank->branch = $data['product_code'];
+                $bank->name = $data['product_town'];
+                $bank->account_no = $data['product_town_code'];
+                $bank->paybill_no = $data['product_color'];
+                $bank->is_current = 2;
+                $bank->save();
+                return redirect('/admin/accounts')->with('flash_message_success', 'Account Created Successfully');
+            }
+            return view('admin.company.add_bank_details');
+        } else {
+            return redirect('/admin')->with('flash_message_error', 'Access denied! Please Login first');
+        }
+    }
+
     public function viewAccounts() {
         if (Session::has('adminSession')) {
             $banksettings = Setting::get();
@@ -76,7 +96,9 @@ class SettingsController extends Controller {
         if (Session::has('adminSession')) {
             if ($request->isMethod('post')) {
                 $data = $request->all();
-                Setting::where(['id' => $id])->update(['bank' => $data['product_name'], 'branch' => $data['product_code'], 'name' => $data['product_town'], 'account_no' => $data['product_town_code'], 'paybill_no' => $data['product_color']]);
+                Setting::where(['id' => $id])->update(['bank' => $data['product_name'],
+                    'branch' => $data['product_code'], 'name' => $data['product_town'],
+                    'account_no' => $data['product_town_code'], 'paybill_no' => $data['product_color']]);
                 return redirect('/admin/accounts')->with('flash_message_success', 'Account updated Successfully');
             } else {
                 $settings = Setting::where(['id' => $id])->first();
